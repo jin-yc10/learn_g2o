8b01c91ad7e0f7f054355ea087f505a151007322
jinyuchao <jinyuchao@sensetime.com>
2016-08-22 21:43:19 +0800

    todo : test fundamental algorithm

--- src/sample.h
+++ src/sample.h
@@ -6,35 +6,37 @@ using namespace std;
 
 class Sample {
 public:
-  static int uniform(int from, int to);
-  static double uniform();
-  static double gaussian(double sigma);
+    static int uniform(int from, int to);
+
+    static double uniform();
+
+    static double gaussian(double sigma);
 };
 
-static double uniform_rand(double lowerBndr, double upperBndr){
-  return lowerBndr + ((double) std::rand() / (RAND_MAX + 1.0)) * (upperBndr - lowerBndr);
+static double uniform_rand(double lowerBndr, double upperBndr) {
+    return lowerBndr + ((double) std::rand() / (RAND_MAX + 1.0)) * (upperBndr - lowerBndr);
 }
 
-static double gauss_rand(double mean, double sigma){
-  double x, y, r2;
-  do {
-    x = -1.0 + 2.0 * uniform_rand(0.0, 1.0);
-    y = -1.0 + 2.0 * uniform_rand(0.0, 1.0);
-    r2 = x * x + y * y;
-  } while (r2 > 1.0 || r2 == 0.0);
-  return mean + sigma * y * std::sqrt(-2.0 * log(r2) / r2);
+static double gauss_rand(double mean, double sigma) {
+    double x, y, r2;
+    do {
+        x = -1.0 + 2.0 * uniform_rand(0.0, 1.0);
+        y = -1.0 + 2.0 * uniform_rand(0.0, 1.0);
+        r2 = x * x + y * y;
+    } while (r2 > 1.0 || r2 == 0.0);
+    return mean + sigma * y * std::sqrt(-2.0 * log(r2) / r2);
 }
 
-int Sample::uniform(int from, int to){
-  return static_cast<int>(uniform_rand(from, to));
+int Sample::uniform(int from, int to) {
+    return static_cast<int>(uniform_rand(from, to));
 }
 
-double Sample::uniform(){
-  return uniform_rand(0., 1.);
+double Sample::uniform() {
+    return uniform_rand(0., 1.);
 }
 
-double Sample::gaussian(double sigma){
-  return gauss_rand(0., sigma);
+double Sample::gaussian(double sigma) {
+    return gauss_rand(0., sigma);
 }
 
 DEFINE_bool(noisy_cam, false, "add noise to camera");
@@ -46,8 +48,10 @@ class ObservationSet {
         int nOb;
         int nPt;
         cv::Size sz;
-        cv::Mat K; bool noisy_cam;
-        cv::Mat pt_3d; bool noisy_pt_3d;
+        cv::Mat K;
+        bool noisy_cam;
+        cv::Mat pt_3d;
+        bool noisy_pt_3d;
         bool noisy_pt_2d;
     };
 
@@ -57,10 +61,12 @@ class ObservationSet {
     };
 
     Params params;
-
+    std::vector<Observe> obs;
 public:
-    ObservationSet(const Params params): params(params){};
-    void build() {
+    ObservationSet(const Params params) : params(params) { };
 
+    void build() {
+        Observe ob;
+        obs.push_back(ob);
     }
 };
